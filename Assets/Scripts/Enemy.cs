1dbd377a09d89fa70a724faf2436e38f88ece313
AzureAD\JuanFernandoGomez <juanfgomeza@gmail.com>
2022-06-07 14:00:48 -0500

    Scoreboard and enemy health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,13 +6,42 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parentSpawnObjects;
+    [SerializeField] int enemyHitPoints = 15;
+    [SerializeField] int enemyHealth = 4;
 
-    void OnParticleCollision(GameObject other) 
+    ScoreBoard scoreBoard;
+
+    void Start() 
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        
+    }
+
+    void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"{this.name} has been hit by {other.gameObject.name}");
-        GameObject vfx = Instantiate(deathVFX,this.transform.position,Quaternion.identity);
+        ProcessHit();
+        if (enemyHealth <= 0)
+        {
+            KillEnemy();
+        }
+        
+    }
+
+    private void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, this.transform.position, Quaternion.identity);
+        vfx.transform.parent = parentSpawnObjects;
+        enemyHealth--;
+        scoreBoard.IncreaseScore(enemyHitPoints);
+    }
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, this.transform.position, Quaternion.identity);
         vfx.transform.parent = parentSpawnObjects;
         Destroy(this.gameObject);
     }
+    
 }
